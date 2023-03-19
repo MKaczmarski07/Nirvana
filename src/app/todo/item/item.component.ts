cc852d7c91085a8b7fd9bb700f8aed028cc19174
Mikołaj Kaczmarski <mikolajkaczmarski07@onet.pl>
2023-03-19 13:45:28 +0100

    new task menu design

--- src/app/todo/item/item.component.ts
+++ src/app/todo/item/item.component.ts
@@ -1,16 +1,39 @@
-import { Component, Input, Output, EventEmitter, HostListener} from '@angular/core';
+import { Component, Input, Output, EventEmitter, HostListener, OnInit} from '@angular/core';
 import { Item } from '../item';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-item',
   templateUrl: './item.component.html',
-  styleUrls: ['./item.component.css']
+  styleUrls: ['./item.component.css'],
+  // animations: [
+  //   trigger(
+  //     'inOutAnimation', 
+  //     [
+  //       transition(
+  //         ':enter', 
+  //         [
+  //           style({  opacity: 0 }),
+  //           animate('.3s linear', 
+  //                   style({  opacity: 1 }))
+  //         ]
+  //       ),
+  //       transition(
+  //         ':leave', 
+  //         [
+  //           style({  opacity: 1 }),
+  //           animate('.3s linear', 
+  //                   style({  opacity: 0 }))
+  //         ]
+  //       )
+  //     ]
+  //   )
+  // ]
 })
+  
 export class ItemComponent {
-  editable = false;
   confirmDeletion = false;
   isOpen = false;
-  fadeInOut = '';
   editedItemValue = '';
 
   @Input() item!: Item;
@@ -18,35 +41,34 @@ export class ItemComponent {
   @Output() remove = new EventEmitter<Item>();
   @Output() saveToLocalStorage = new EventEmitter<Item>()
 
-
-  @HostListener('document:click', ['$event']) //Remove the dropdown menu when clicking outside of the item component 
-  onClick(event: MouseEvent) {
-    const target = event.target as HTMLElement;
-    if ((!target.closest('.item-componet'))) {
-      if (this.isOpen) { //Toggle the fade out animation only if the dropdown menu is open
-        this.isOpen = false;
-        this.fadeInOut = 'fadeOut';
-      }
-      this.editable = false;
-      this.confirmDeletion = false;
-    }
+  ngOnInit() {
+    this.editedItemValue = this.item.description;
   }
+
+
+  // @HostListener('document:click', ['$event']) //Remove the dropdown menu when clicking outside of the item component 
+  // onClick(event: MouseEvent) {
+  //   const target = event.target as HTMLElement;
+  //   if ((!target.closest('.task-menu')) && this.isOpen) {
+  //     if (this.isOpen && !target.closest('.btn-options')) {
+  //       this.isOpen = false;
+  //       this.confirmDeletion = false;
+  //       this.saveItem(this.editedItemValue)
+  //     }
+      
+  //   }
+  // }
 
   markAsDone() {
     this.item.done = !this.item.done
     this.saveToLocalStorage.emit()
   }
 
-  editItem() { 
-    this.editable = !this.editable
-    this.editedItemValue = this.item.description; //display the current description in the input field
-    if(this.editable) this.confirmDeletion = false; //If the user is editing the item, the confirm deletion option will be removed
-  }
-
+  
   saveItem(description: string) {
+    //prevent from saving empty task
+    if (!description) return; 
     //Save new description and save item in the local storage
-    if (!description) return;
-    this.editable = false;
     this.item.description = description;
     this.saveToLocalStorage.emit()
   }
@@ -56,20 +78,12 @@ export class ItemComponent {
     this.saveToLocalStorage.emit()
   }
 
-  deleteItem() { 
-    this.confirmDeletion = !this.confirmDeletion
-    if(this.confirmDeletion) this.editable = false; //If the user is confirming the deletion, the edit option will be removed
-  }
 
-  toggleOptions() { //Toggle the dropdown menu
+  toggleOptions() {
+    //Toggle the dropdown menu
     this.isOpen = !this.isOpen;
-    if (!this.isOpen) this.editable = false;
     if(!this.isOpen) this.confirmDeletion = false
 
-    if (this.isOpen) { //Toggle the fade in/out animation
-      this.fadeInOut = 'fadeIn';
-    }else this.fadeInOut = 'fadeOut';
-    
   }
 
   
