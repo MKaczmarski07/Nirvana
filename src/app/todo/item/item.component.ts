b01a6b159f0f4ff986b592f6a9b7ed3304a42b9f
Mikołaj Kaczmarski <mikolajkaczmarski07@onet.pl>
2023-03-29 17:08:50 +0200

    Improved the removal of empty tasks

--- src/app/todo/item/item.component.ts
+++ src/app/todo/item/item.component.ts
@@ -1,13 +1,17 @@
-import { Component, Input, Output, EventEmitter, HostListener, OnInit} from '@angular/core';
+import {
+  Component,
+  Input,
+  Output,
+  EventEmitter,
+  HostListener,
+} from '@angular/core';
 import { Item } from '../item';
 
 @Component({
   selector: 'app-item',
   templateUrl: './item.component.html',
   styleUrls: ['./item.component.css'],
-  
 })
-  
 export class ItemComponent {
   confirmDeletion = false;
   isOpen = false;
@@ -16,34 +20,38 @@ export class ItemComponent {
   @Input() item!: Item;
   @Input() newItem!: string;
   @Output() remove = new EventEmitter<Item>();
-  @Output() saveToLocalStorage = new EventEmitter<Item>()
+  @Output() saveToLocalStorage = new EventEmitter<Item>();
 
   ngOnInit() {
     this.editedItemValue = this.item.description;
   }
 
-
   markAsDone() {
-    this.item.done = !this.item.done
-    this.saveToLocalStorage.emit()
+    this.item.done = !this.item.done;
+    this.saveToLocalStorage.emit();
   }
 
-  
   saveItem(description: string) {
     //prevent from saving empty task
     if (this.editedItemValue.trim() === '') return;
     //Save new description and save item in the local storage
     this.item.description = description;
-    this.saveToLocalStorage.emit()
+    this.saveToLocalStorage.emit();
   }
+
+  @HostListener('document:click', ['$event']) 
+  //remove the empty task only when the user clicks outside of the task
+  onClick(event: MouseEvent) {
+    const target = event.target as HTMLElement;
+    if ((!target.closest('.item-component')) && this.editedItemValue.trim()==='') {
+      this.remove.emit();
+      }
+  }
+  
+  
 
   markAsImportant() {
-    this.item.isImportant = !this.item.isImportant
-    this.saveToLocalStorage.emit()
+    this.item.isImportant = !this.item.isImportant;
+    this.saveToLocalStorage.emit();
   }
-
-  
-
-  
-
 }
