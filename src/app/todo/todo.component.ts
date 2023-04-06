2479f0bb246bc8eb2ef3e6c226d65d4ac4f9dce0
Mikołaj Kaczmarski <mikolajkaczmarski07@onet.pl>
2023-04-06 14:13:27 +0200

    Added graphics for empty cases

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,67 +1,64 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Item } from './item';
 import { CustomDateService } from '../date.service';
-
-
 
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
-  styleUrls: ['./todo.component.css']
+  styleUrls: ['./todo.component.css'],
 })
 export class TodoComponent implements OnInit {
-  constructor(
-    private dateService: CustomDateService
-  ) { }
+  constructor(private dateService: CustomDateService) {}
 
   tasks: Item[] = [];
   dropdownOpen = false;
   currentDay = this.dateService.getCurrentDate();
   fadeInOut = '';
   filter: 'all' | 'active' | 'done' | 'important' = 'all';
-  
+  numberOfTasks = 0;
+
   ngOnInit() {
-    this.dateService.getCurrentDate()
+    this.dateService.getCurrentDate();
     const savedTasks = localStorage.getItem('tasks');
     if (savedTasks) {
       this.tasks = JSON.parse(savedTasks);
     }
+    this.numberOfTasks = this.tasks.length;
   }
 
   saveToLocalStorage() {
     localStorage.setItem('tasks', JSON.stringify(this.tasks));
   }
-  
+
   addItem(description: string) {
     //prevent from saving empty task
-    if (description.trim() === '') return; 
+    if (description.trim() === '') return;
     //add item to the top of the list
-    this.tasks.unshift({ 
+    this.tasks.unshift({
       description,
       done: false,
-      isImportant: false
-    })
-    
-    this.saveToLocalStorage()
+      isImportant: false,
+    });
+    this.numberOfTasks = this.tasks.length;
+    this.saveToLocalStorage();
   }
 
   removeItem(item: Item) {
     this.tasks.splice(this.tasks.indexOf(item), 1);
-    this.saveToLocalStorage()
+    this.numberOfTasks = this.tasks.length;
+    this.saveToLocalStorage();
   }
-  
+
   tasksfiltering() {
-    if (this.filter==='active') {
-      return (item:Item) => !item.done
-    }else if (this.filter==='done') {
-      return (item:Item) => item.done
-    } else if (this.filter==='important') {
-      return (item:Item) => item.isImportant
+    if (this.filter === 'active') {
+      return (item: Item) => !item.done;
+    } else if (this.filter === 'done') {
+      return (item: Item) => item.done;
+    } else if (this.filter === 'important') {
+      return (item: Item) => item.isImportant;
     }
-    return (item:Item) => item
+    return (item: Item) => item;
   }
-  
-  
 
   // taskSorting() {
   //   this.tasks.sort((a, b) => {
@@ -72,13 +69,4 @@ export class TodoComponent implements OnInit {
   //     }
   //   });
   // }
-  
-
-
-  
 }
-
-
-
-
-
